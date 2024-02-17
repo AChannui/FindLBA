11fc665ac4a3a6611f4f6f2ae1d2e236b361b154
Alexander Chan-Nui <alex@tiny.org>
2024-02-16 19:28:05 -0600

    finished proj-part2 assignment. added reading super block part. also fixed parsing of num_sectors and lba.

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <arpa/inet.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -21,6 +22,14 @@ typedef struct {
     uint32_t num_sectors;
 
 } partition_t ;
+
+typedef struct {
+    uint32_t inodes;
+    uint32_t blocks;
+    uint16_t magic_sig;
+    uint32_t block_size;
+
+} super_block_t;
 
 chs_t parseChs(const uint8_t* source){
     chs_t result;
@@ -45,20 +54,33 @@ uint32_t parseNum_sectors(const uint8_t* source){
     return result;
 }
 
-partition_t parsePartition(uint8_t* source){
+partition_t parsePartition(const uint8_t* source){
     partition_t result;
     result.boot_flag = source[0];
     result.start_chs = parseChs(&source[1]);
     result.type_code = source[4];
     result.end_chs = parseChs(&source[5]);
-    result.lba = parseNum_sectors(&source[8]);
+    result.lba = *(uint32_t*) (&source[8]);
+    //result.lba = parseNum_sectors(&source[8]);
     //ntohl doesn't work wrong order
     //result.lba = ntohl(*((uint32_t*) &source[8]));
     // this works because of machine endianness
     //result.lba = (*((uint32_t*) &source[8]));
-    result.num_sectors = parseNum_sectors(&source[12]);
+    result.num_sectors = *(uint32_t*) (&source[12]);
+    //result.num_sectors = parseNum_sectors(&source[12]);
     return result;
 }
+
+super_block_t parseSuperBlock(const uint8_t* source){
+    super_block_t result;
+    result.inodes = *(uint32_t*) (&source[0]);
+    result.blocks = *(uint32_t*) (&source[4]);
+    uint32_t temp = *(uint32_t*) (&source[0x18]);
+    result.block_size = 1 << (temp + 10);
+    result.magic_sig = *(uint16_t*) (&source[0x38]);
+    return result;
+}
+
 
 uint32_t getPartAddr(int fd){
     const int BLOCK_SIZE = 512;
@@ -79,8 +101,19 @@ uint32_t getPartAddr(int fd){
 }
 
 void readSuperBlock(int fd, int off){
+    const int BLOCK_SIZE = 4096;
+    uint8_t buff[BLOCK_SIZE];
     lseek(fd, off, SEEK_SET);
-
+    ssize_t read_check = read(fd, buff, BLOCK_SIZE);
+    if(read_check != BLOCK_SIZE){
+        printf("Read not enough bytes from super block %zd\n", read_check);
+        exit(EXIT_FAILURE);
+    }
+   super_block_t super_block = parseSuperBlock(buff);
+    printf("Magic signature : %x\n", super_block.magic_sig);
+    printf("Size of block : %d\n", super_block.block_size);
+    printf("Total number of blocks : %d\n", super_block.blocks);
+    printf("Total number of inodes : %d\n", super_block.inodes);
 }
 
 void help(const char *name){
@@ -118,6 +151,7 @@ int main(int argc, char *argv[]) {
 
     // get partition address
     uint32_t part_address = getPartAddr(drive_fd);
+    readSuperBlock(drive_fd, part_address + 1024);
 
 
 
