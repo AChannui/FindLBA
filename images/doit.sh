387284578f27990d8fd192bc4b6125f34da6100c
Alexander Chan-Nui <alex@tiny.org>
2024-03-20 02:30:37 -0500

    adding MakeFile that creates disk images.

--- images/doit.sh
+++ images/doit.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-FILE=${1:-file-50k}
-FSTYPE=${2:-ext3}
+function run_test {
+    local FILE=${1:-file-50k}
+    local FSTYPE=${2:-ext3}
 
-./create_image.sh ${FSTYPE}
-make mount
-cp ${FILE} mnt
-make unmount
-/tmp/tmp.VD00t0lx7J/cmake-build-virtualbox-opensuse/Assignment2 -d disk.img
+    ./create_image.sh ${FSTYPE}
+    make mount
+    cp ${FILE} mnt
+    make unmount
+    /mnt/c/Users/alex/College/CS4398/Assignment2/cmake-build-wsl-opensuse/Assignment2 -d disk.img
+}
+
+for file in file-{50k,5M,5G}; do 
+    for fstype in ext{2,3}; do
+	run_test $file $fstype | tee output-$file-$fstype.txt
+    done
+done
+
